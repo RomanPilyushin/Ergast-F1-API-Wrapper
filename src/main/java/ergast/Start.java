af803966a7d06fac8075358d783f01ca3bade3b8
Roman <rpilyushin@gmail.com>
2023-11-19 13:54:06 +0200

    Add saving result to CSV

--- src/main/java/ergast/Start.java
+++ src/main/java/ergast/Start.java
@@ -1,6 +1,11 @@
 package ergast;
 
 import ergast.objects.RacePitStops;
+import ergast.objects.Schedule;
+import ergast.objects.Season;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -8,11 +13,31 @@ public class Start {
     public static void main(String[] args) {
         try {
             Ergast ergast = new Ergast(2016, 100, 2);
-            List<RacePitStops> results = ergast.getRacePitStops(21);
+            List<RacePitStops> racePitStopsList = ergast.getRacePitStops(21);
+            writeListToCSV(racePitStopsList);
 
-            results.forEach(result -> System.out.println(result));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static <T> void writeListToCSV(List<T> list) throws IOException {
+        if (list == null || list.isEmpty()) {
+            System.out.println("List is empty or null.");
+            return;
+        }
+
+        String className = list.get(0).getClass().getSimpleName();
+        String fileName = className + ".csv";
+
+        try (
+                FileWriter writer = new FileWriter(fileName);
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("Data"))
+        ) {
+            for (T item : list) {
+                csvPrinter.printRecord(item.toString());
+            }
+            csvPrinter.flush();
         }
     }
 }
